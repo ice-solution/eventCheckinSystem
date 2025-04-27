276856baec404cb50e08008864a35f6eaf6e076e
Mark Ng <mark.ng@makerville.hk>
2025-04-27 09:47:20 +0800

    + email template

--- model/EmailTemplate.js
+++ model/EmailTemplate.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const emailTemplateSchema = new mongoose.Schema({
     subject: { type: String, required: true }, // 電子郵件主題
-    body: { type: String, required: true },    // 電子郵件內容
+    content: { type: String,  default: "", },    // 電子郵件內容
     created_at: { type: Date, default: Date.now }, // 創建時間
     modified_at: { type: Date, default: Date.now }  // 修改時間
 });
