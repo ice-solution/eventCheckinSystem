e8d2a28382952aa4a9db4baf0b6984fa575f1589
Leung Keith <icesolution0321@gmail.com>
2026-01-03 17:47:54 +0900

    update fields

--- model/EmailTemplate.js
+++ model/EmailTemplate.js
@@ -4,7 +4,11 @@ const emailTemplateSchema = new mongoose.Schema({
     eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event'}, // 事件ID
     subject: { type: String, required: true }, // 電子郵件主題
     content: { type: String,  default: "", },    // 電子郵件內容
-    type: { type: String, default: "welcome" }, // 電子郵件類型
+    type: { 
+        type: String, 
+        enum: ['invitation', 'welcome', 'knowledge', 'reminder'], 
+        default: "welcome" 
+    }, // 電子郵件類型
     created_at: { type: Date, default: Date.now }, // 創建時間
     modified_at: { type: Date, default: Date.now }  // 修改時間
 });
